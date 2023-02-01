3070fbc9376ed21c1376c5b80f8d95ab6be46b15
Semicolon-GD <maerken@icloud.com>
2023-02-01 09:41:19 +0300

    Added walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
-
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal")*_speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal,rigidbody2D.velocity.y);
+        var animator = GetComponent<Animator>();
+
+        animator.SetBool("Walk", horizontal!=0);
     }
 }
