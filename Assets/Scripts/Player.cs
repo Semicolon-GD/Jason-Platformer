a61f64d0fe813d9e7cf75cb9b4a6a6afcc7e32d3
Semicolon-GD <maerken@icloud.com>
2023-02-01 09:51:33 +0300

    Added jump to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
+    [SerializeField] float _jumpForce = 200;
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal")*_speed;
@@ -20,6 +21,11 @@ public class Player : MonoBehaviour
             var spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.flipX = horizontal < 0;
         }
-        
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.AddForce(Vector2.up*_jumpForce);
+        }
     }
 }
