12cb583057dd380c8bfdc0b1cac9a4c55f1339eb
Semicolon-GD <maerken@icloud.com>
2023-02-02 18:49:19 +0300

    changed jump to use velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
-    [SerializeField] float _jumpForce = 200;
+    [SerializeField] float _jumpVelocity = 10;
     [SerializeField] int _maxJumps = 2;
     [SerializeField] Transform _feet;
+    [SerializeField] float _downPull=5;
 
     Vector2 _startPosition;
     int _jumpsRemaining;
+    float _fallTimer;
     
 
     void Start()
@@ -21,6 +23,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        
         var horizontal = Input.GetAxis("Horizontal")*_speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         if (Mathf.Abs(horizontal)>=1)
@@ -40,22 +43,28 @@ public class Player : MonoBehaviour
         }
 
 
-        if (Input.GetButtonDown("Fire1") && _jumpsRemaining>0)
+        if (Input.GetButtonDown("Jump") && _jumpsRemaining>0)
         {
-            rigidbody2D.AddForce(Vector2.up*_jumpForce);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,_jumpVelocity);
             _jumpsRemaining--;
+            _fallTimer = 0;
+        }
+        var hit = Physics2D.OverlapCircle(_feet.position, .1f, LayerMask.GetMask("Default"));
+        bool isGrounded = hit != null;
+
+        if (isGrounded)
+        {
+            _fallTimer = 0;
+            _jumpsRemaining = _maxJumps;
+        }
+        else
+        {
+            _fallTimer += Time.deltaTime;
+            var _downForce = _downPull * _fallTimer * _fallTimer;
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y-_downForce);
         }
     }
 
-     void OnCollisionEnter2D(Collision2D collision)
-    {
-        var hit = Physics2D.OverlapCircle(_feet.position, .1f, LayerMask.GetMask("Default"));
-        if (hit!=null)
-        {
-            _jumpsRemaining = _maxJumps;
-        }
-          
-    }
     internal void ResetToStart()
     {
         transform.position = _startPosition;
