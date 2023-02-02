ccfda985bf38117401af627217af75f1bd5b3324
Semicolon-GD <maerken@icloud.com>
2023-02-02 08:07:34 +0300

    Slippery ground and collider changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,11 @@ public class Player : MonoBehaviour
     {
         var horizontal = Input.GetAxis("Horizontal")*_speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = new Vector2(horizontal,rigidbody2D.velocity.y);
+        if (Mathf.Abs(horizontal)>=1)
+        {
+            rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        }
+        
         var animator = GetComponent<Animator>();
 
         animator.SetBool("Walk", horizontal!=0);
