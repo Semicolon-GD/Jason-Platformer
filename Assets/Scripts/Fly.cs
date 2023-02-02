c0587669aed0b14665f73fc15d62f1a1d1a4fc8e
Semicolon-GD <maerken@icloud.com>
2023-02-02 09:51:16 +0300

    added direction and speed to fly

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
-    Vector3 _startPosition;
-    Vector2 _direction=Vector2.up;
+    Vector2 _startPosition;
+    [SerializeField]Vector2 _direction=Vector2.up;
+    [SerializeField] float _maxDistance = 2;
+    [SerializeField] float _speed = 2;
 
     void Start()
     {
@@ -14,10 +16,11 @@ public class Fly : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(_direction * Time.deltaTime);
+        transform.Translate(_direction.normalized * Time.deltaTime*_speed);
         var distance = Vector2.Distance(_startPosition,transform.position);
-        if (distance>2)
+        if (distance> _maxDistance)
         {
+            transform.position = _startPosition + (_direction.normalized * _maxDistance);
             _direction *= -1;
         }
     }
