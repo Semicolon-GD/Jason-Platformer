03978f5a271d30effd96bf79aa4de1f6ba6eec0e
Semicolon-GD <maerken@icloud.com>
2023-02-03 08:51:01 +0300

    Level Progression

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Flag : MonoBehaviour
 {
+   [SerializeField] string _sceneName;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
@@ -12,5 +15,8 @@ public class Flag : MonoBehaviour
 
         var animator = GetComponent<Animator>();
         animator.SetTrigger("Raise");
+
+
+        SceneManager.LoadScene(_sceneName);
     }
 }
