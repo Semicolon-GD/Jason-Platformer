efcabbe88d2c42f2f00960424213d2a323426404
Semicolon-GD <maerken@icloud.com>
2023-02-04 10:37:58 +0300

    Refactored player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,54 +15,50 @@ public class Player : MonoBehaviour
     int _jumpsRemaining;
     float _fallTimer;
     float _jumpTimer;
-    
+    Rigidbody2D _rigidbody2D;
+    Animator _animator;
+    SpriteRenderer _spriteRenderer;
+    float _horizontal;
+    bool _isGrounded;
 
     void Start()
     {
         _startPosition = transform.position;
         _jumpsRemaining = _maxJumps;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        var hit = Physics2D.OverlapCircle(_feet.position, .1f, LayerMask.GetMask("Default","Mushroom"));
-        bool isGrounded = hit != null;
+        UpdateIsGrounded();
+        ReadHorizontalInput();
+        MoveHorizontal();
 
-        var horizontal = Input.GetAxis("Horizontal")*_speed;
-        var rigidbody2D = GetComponent<Rigidbody2D>();
-        if (Mathf.Abs(horizontal)>=1)
-        {
-            rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
-        }
-        
-        var animator = GetComponent<Animator>();
+        UpdateAnimator();
 
-        animator.SetBool("Walk", horizontal!=0);
+        UpdateSpriteDirection();
 
-
-        if (horizontal!=0)
-        {
-            var spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.flipX = horizontal < 0;
-        }
+        if (ShouldStartJump())
+            Jump();
+        else if (ShouldContinueJump())
+            ContinueJump();
+        JumpTimerControll();
+        ManageFall();
+    }
 
 
-        if (Input.GetButtonDown("Jump") && _jumpsRemaining>0)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,_jumpVelocity);
-            _jumpsRemaining--;
-            _fallTimer = 0;
-            _jumpTimer = 0;
-        }
-        else if (Input.GetButton("Jump") && _jumpTimer<=_maxJumpDuration)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
-            _fallTimer = 0;
-           
-        }
+
+    #region Methods
+    void JumpTimerControll()
+    {
         _jumpTimer += Time.deltaTime;
+    }
 
-        if (isGrounded && _fallTimer>0)
+    void ManageFall()
+    {
+        if (_isGrounded && _fallTimer > 0)
         {
             _fallTimer = 0;
             _jumpsRemaining = _maxJumps;
@@ -71,12 +67,71 @@ public class Player : MonoBehaviour
         {
             _fallTimer += Time.deltaTime;
             var _downForce = _downPull * _fallTimer * _fallTimer;
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y-_downForce);
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y - _downForce);
         }
+    }
+    void ContinueJump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpVelocity);
+        _fallTimer = 0;
+    }
+
+    bool ShouldContinueJump()
+    {
+        return Input.GetButton("Jump") && _jumpTimer <= _maxJumpDuration;
+    }
+
+    void Jump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpVelocity);
+        _jumpsRemaining--;
+        _fallTimer = 0;
+        _jumpTimer = 0;
+    }
+
+    bool ShouldStartJump()
+    {
+        return Input.GetButtonDown("Jump") && _jumpsRemaining > 0;
+    }
+
+    void MoveHorizontal()
+    {
+        if (Mathf.Abs(_horizontal) >= 1)
+        {
+            _rigidbody2D.velocity = new Vector2(_horizontal, _rigidbody2D.velocity.y);
+        }
+    }
+
+    void ReadHorizontalInput()
+    {
+        _horizontal = Input.GetAxis("Horizontal") * _speed;
+    }
+
+    void UpdateSpriteDirection()
+    {
+        if (_horizontal != 0)
+        {
+
+            _spriteRenderer.flipX = _horizontal < 0;
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        bool walking = _horizontal != 0;
+        _animator.SetBool("Walk", _horizontal != 0);
+    }
+
+    void UpdateIsGrounded()
+    {
+        var hit = Physics2D.OverlapCircle(_feet.position, .1f, LayerMask.GetMask("Default", "Mushroom"));
+        _isGrounded = hit != null; 
     }
 
     internal void ResetToStart()
     {
         transform.position = _startPosition;
     }
+
+    #endregion
 }
