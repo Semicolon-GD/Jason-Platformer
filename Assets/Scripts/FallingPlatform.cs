334971c102c9fd9e6050e4aba0c50ac6fe3d5948
Semicolon-GD <maerken@icloud.com>
2023-07-16 20:46:12 +0300

    falling platform

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public bool PlayerInside;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+        PlayerInside = true;
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+        PlayerInside = false;
+    }
+}
