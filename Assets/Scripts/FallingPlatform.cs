50a1381f62c82ecdd3363471bd71c52a6d1daede
Semicolon-GD <maerken@icloud.com>
2023-07-16 21:09:39 +0300

    Added wiggle function

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,58 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
+    HashSet<Player> _playersInTrigger = new HashSet<Player>();
+    Coroutine _coroutine;
+    Vector3 _initialPosition;
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
+
+        _playersInTrigger.Add(player);
+
         PlayerInside = true;
+        if (_playersInTrigger.Count == 1)
+            _coroutine=StartCoroutine(WiggleAndFall());
     }
 
+    IEnumerator WiggleAndFall()
+    {
+        yield return new WaitForSeconds(0.25f);
+        float wiggleTimer = 0;
+
+        while (wiggleTimer<1f)
+        {
+            float randomX = UnityEngine.Random.Range(-0.05f, 0.01f);
+            float randomY = UnityEngine.Random.Range(-0.05f, 0.01f);
+            transform.position = _initialPosition+ new Vector3(randomX, randomY);
+            float randomDelay = UnityEngine.Random.Range(0.005f, 0.01f);
+            yield return new WaitForSeconds(randomDelay);
+            wiggleTimer += randomDelay;
+        }
+        
+        yield return new WaitForSeconds(3f);
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
-        PlayerInside = false;
+
+        _playersInTrigger.Remove(player);
+        if (_playersInTrigger.Count == 0)
+        {
+            PlayerInside = false;
+            StopCoroutine(_coroutine);
+        }
+            
     }
 }
