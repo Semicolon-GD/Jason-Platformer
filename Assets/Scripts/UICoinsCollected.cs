a3d846d06955fde0d556733a14bedf3f1184ef16
Semicolon-GD <maerken@icloud.com>
2023-07-16 20:35:18 +0300

    code update for TMP

--- Assets/Scripts/UICoinsCollected.cs
+++ Assets/Scripts/UICoinsCollected.cs
@@ -1,19 +1,19 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UICoinsCollected : MonoBehaviour
 {
-    Text _text;
+    TMP_Text _text;
 
     // Start is called before the first frame update
     void Start()
     {
-        _text = GetComponent<Text>();   
+        _text = GetComponent<TMP_Text>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        _text.text = Coin.CoinsCollected.ToString();  
+        _text.SetText(Coin.CoinsCollected.ToString());  
     }
 }
