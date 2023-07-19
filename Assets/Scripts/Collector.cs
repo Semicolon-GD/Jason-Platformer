1c7c7c31feaf570e0c9a90dfa13823c5d6e9c0c1
Semicolon-GD <maerken@icloud.com>
2023-07-19 10:32:45 +0300

    added UI elements to collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,18 +1,30 @@
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
+    TMP_Text _remainingText;
+
+    private void Start()
+    {
+        _remainingText = GetComponentInChildren<TMP_Text>();
+    }
 
     private void Update()
     {
+        int countRemaining = 0;
         foreach (var collectible in _collectibles)
         {
             if (collectible.isActiveAndEnabled)
-                return;
+                countRemaining++;
         }
 
+        _remainingText?.SetText(countRemaining.ToString());
+
+        if (countRemaining > 0)
+            return;
         Debug.Log("got all gems");
     }
 }
