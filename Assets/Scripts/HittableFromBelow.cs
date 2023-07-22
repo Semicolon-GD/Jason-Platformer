ae2a97232ee31be8271c831356683c7fadc95a7d
Semicolon-GD <maerken@icloud.com>
2023-07-22 14:23:53 +0300

    used inheritence for box animation

--- Assets/Scripts/HittableFromBelow.cs
+++ Assets/Scripts/HittableFromBelow.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public class HittableFromBelow : MonoBehaviour
 {
     [SerializeField] protected Sprite _usedSprite;
+    Animator _animator;
 
     protected virtual bool CanUse => true;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,11 +22,18 @@ public class HittableFromBelow : MonoBehaviour
             return;
         if (collision.contacts[0].normal.y > 0)
         {
+            PlayAnimation();
             Use();
 
             if (CanUse == false)
                 GetComponent<SpriteRenderer>().sprite = _usedSprite;
         }
+    }
+
+    void PlayAnimation()
+    {
+        if (_animator != null)
+            _animator.SetTrigger("Use");
     }
 
     protected virtual void Use()
