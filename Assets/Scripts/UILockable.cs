1980ffde32be8166d343fa87bd17fa44e0024d97
Semicolon-GD <maerken@icloud.com>
2023-07-23 16:42:16 +0300

    hid locked levels with UILockable

---
+++ Assets/Scripts/UILockable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class UILockable : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var startButton = GetComponent<UIStartLevelButton>();
+        string key = startButton.LevelName + " Unlocked"; //"Level1 Unlocked"
+        int unlocked = PlayerPrefs.GetInt(key, 0);
+
+        if (unlocked == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
--- Assets/Scripts/UIStartLevelButton.cs
+++ Assets/Scripts/UIStartLevelButton.cs
@@ -2,15 +2,16 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UIStartLevelButton : MonoBehaviour
+public partial class UIStartLevelButton : MonoBehaviour
 {
     [SerializeField] string _levelName;
+
+    public string LevelName => _levelName;
 
     public void LoadLevel()
     {
         SceneManager.LoadScene(_levelName);
     }
-
     //private void OnValidate()
     //{
     //    GetComponentInChildren<TMP_Text>()?.SetText(_levelName);
