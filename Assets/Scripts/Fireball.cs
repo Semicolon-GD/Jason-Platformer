4b6ff9dc3c76b04acb1744d96431c13654e0d54e
Semicolon-GD <maerken@icloud.com>
2023-07-25 22:04:05 +0300

    set direction to fireball

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    [SerializeField] float _launchForce=5;
+    [SerializeField] float _launchForce=8;
+    [SerializeField] float _bounceForce = 5f;
+    int _bounceRemaining=3;
+    Rigidbody2D _rigidbody;
+
+    public float Direction { get; set; }
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(_launchForce, 0);
+        _rigidbody = GetComponent<Rigidbody2D>();
+       _rigidbody.velocity= new Vector2(_launchForce* Direction,_bounceForce);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        _bounceRemaining--;
+        if (_bounceRemaining < 0)
+            Destroy(gameObject);
+        else
+            _rigidbody.velocity = new Vector2(_launchForce * Direction, _bounceForce);
     }
 }
--- Assets/Scripts/FireballLauncher.cs
+++ Assets/Scripts/FireballLauncher.cs
@@ -1,25 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FireballLauncher : MonoBehaviour
 {
     [SerializeField] Fireball _fireballPrefab;
+    [SerializeField] float _fireRate;
+
     Player _player;
     string _fireButton;
+    string _horizontalAxis;
+    float _nextFireTime;
+    
 
-   void Start()
+    void Start()
     {
         _player = GetComponent<Player>();
         _fireButton = $"P{_player.PlayerNumber}Fire";
+        _horizontalAxis = $"P{_player.PlayerNumber}Horizontal";
         
     }
 
     void Update()
     {
-        if (Input.GetButtonDown(_fireButton))
+        if (Input.GetButtonDown(_fireButton) && Time.time >= _nextFireTime)
         {
-            Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+            var horizontal = Input.GetAxis(_horizontalAxis);
+            Fireball fireball= Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+            fireball.Direction = horizontal >= 0 ? 1f : -1f;
+            _nextFireTime = Time.time + _fireRate;
         }
     }
 }
